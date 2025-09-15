722d9cb9158d33f4c8c65311f26933af53025da5
kumimasaki <masaki.kmkm@gmail>
2025-09-16 08:46:47 +0900

    プロジェクト作成

---
+++ TeamAProject/src/main/java/project/com/TeamAProjectApplication.java
@@ -0,0 +1,13 @@
+package project.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamAProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamAProjectApplication.class, args);
+	}
+
+}
---
+++ TeamAProject/src/test/java/project/com/TeamAProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package project.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamAProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
